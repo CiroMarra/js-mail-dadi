fceeff89d83b55d3267e5eadcced1932cf755426
CiroMarra <tensha93@gmail.com>
2024-03-08 16:30:51 +0100

    init project - found email

---
+++ email/js/script.js
@@ -0,0 +1,7 @@
+// Chiedi all’utente la sua email,
+// controlla che sia nella lista di chi può accedere,
+// stampa un messaggio appropriato sull’esito del controllo.
+// Per la soluzione di questo esercizio NON è consentito utilizzare .includes di js!
+
+
+
